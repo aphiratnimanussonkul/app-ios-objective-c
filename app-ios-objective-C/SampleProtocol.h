8fea78c9f3056c1c72f64837baec4915aac3f542
Aphirat Nimanussonkul <tata@odds.team>
2020-02-19 10:51:51 +0700

    init app objective C

---
+++ app-ios-objective-C/SampleProtocol.h
@@ -0,0 +1,17 @@
+//
+//  SampleProtocol.h
+//  app-ios-objective-C
+//
+//  Created by ODDS on 19/2/2563 BE.
+//  Copyright © 2563 ODDS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol SampleProtocol <NSObject>
+
+@end
+
+NS_ASSUME_NONNULL_END
